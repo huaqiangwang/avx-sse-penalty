b9a8d48f7b8b01a63766449e130fe44ee7e7219d
Wang Huqiang <huaqiang.wang@intel.com>
2022-10-21 17:07:41 +0800

    add interface of change code with .s file

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 all:
 	g++ ast-no-penalty.cc -o ast-no-penalty
 	g++ ast-penalty.cc -o ast-penalty
+	g++ ast-penalty_gs.s -o ast-penalty_gs
 run:
 	@echo "Run ast-penalty @avx2 on CPU 1, try to capture "ASSISTS.SSE_AVX_MIX" event"
 	emon -t0 -C "ASSISTS.SSE_AVX_MIX"  /usr/bin/taskset -c 1 ./ast-penalty |grep 'ASSISTS.SSE_AVX_MIX'
@@ -14,9 +15,10 @@ run:
 	@echo "Run ast-no-penalty @avx512 on CPU 1, try to capture "ASSISTS.SSE_AVX_MIX" event"
 	emon -t0 -C "ASSISTS.SSE_AVX_MIX"  /usr/bin/taskset -c 1 ./ast-no-penalty avx512 |grep 'ASSISTS.SSE_AVX_MIX'
 
+
 sde:
 	sde64 -ast -debugtrace -- ./ast-penalty
 
 clean:
-	- rm ast-penalty ast-no-penalty sde* pin*
+	- rm ast-penalty ast-no-penalty ast-penalty_gs sde* pin*
 
---
+++ ast-penalty_gs.s
@@ -0,0 +1,374 @@
+	.file	"ast-penalty.cc"
+	.text
+	.section	.rodata
+	.type	_ZStL19piecewise_construct, @object
+	.size	_ZStL19piecewise_construct, 1
+_ZStL19piecewise_construct:
+	.zero	1
+	.section	.text._ZStanSt13_Ios_FmtflagsS_,"axG",@progbits,_ZStanSt13_Ios_FmtflagsS_,comdat
+	.weak	_ZStanSt13_Ios_FmtflagsS_
+	.type	_ZStanSt13_Ios_FmtflagsS_, @function
+_ZStanSt13_Ios_FmtflagsS_:
+.LFB1114:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	movl	-4(%rbp), %eax
+	andl	-8(%rbp), %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1114:
+	.size	_ZStanSt13_Ios_FmtflagsS_, .-_ZStanSt13_Ios_FmtflagsS_
+	.section	.text._ZStorSt13_Ios_FmtflagsS_,"axG",@progbits,_ZStorSt13_Ios_FmtflagsS_,comdat
+	.weak	_ZStorSt13_Ios_FmtflagsS_
+	.type	_ZStorSt13_Ios_FmtflagsS_, @function
+_ZStorSt13_Ios_FmtflagsS_:
+.LFB1115:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	movl	-4(%rbp), %eax
+	orl	-8(%rbp), %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1115:
+	.size	_ZStorSt13_Ios_FmtflagsS_, .-_ZStorSt13_Ios_FmtflagsS_
+	.section	.text._ZStcoSt13_Ios_Fmtflags,"axG",@progbits,_ZStcoSt13_Ios_Fmtflags,comdat
+	.weak	_ZStcoSt13_Ios_Fmtflags
+	.type	_ZStcoSt13_Ios_Fmtflags, @function
+_ZStcoSt13_Ios_Fmtflags:
+.LFB1117:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -4(%rbp)
+	movl	-4(%rbp), %eax
+	notl	%eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1117:
+	.size	_ZStcoSt13_Ios_Fmtflags, .-_ZStcoSt13_Ios_Fmtflags
+	.section	.text._ZStoRRSt13_Ios_FmtflagsS_,"axG",@progbits,_ZStoRRSt13_Ios_FmtflagsS_,comdat
+	.weak	_ZStoRRSt13_Ios_FmtflagsS_
+	.type	_ZStoRRSt13_Ios_FmtflagsS_, @function
+_ZStoRRSt13_Ios_FmtflagsS_:
+.LFB1118:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movq	%rdi, -8(%rbp)
+	movl	%esi, -12(%rbp)
+	movq	-8(%rbp), %rax
+	movl	(%rax), %eax
+	movl	-12(%rbp), %edx
+	movl	%edx, %esi
+	movl	%eax, %edi
+	call	_ZStorSt13_Ios_FmtflagsS_
+	movq	-8(%rbp), %rdx
+	movl	%eax, (%rdx)
+	movq	-8(%rbp), %rax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1118:
+	.size	_ZStoRRSt13_Ios_FmtflagsS_, .-_ZStoRRSt13_Ios_FmtflagsS_
+	.section	.text._ZStaNRSt13_Ios_FmtflagsS_,"axG",@progbits,_ZStaNRSt13_Ios_FmtflagsS_,comdat
+	.weak	_ZStaNRSt13_Ios_FmtflagsS_
+	.type	_ZStaNRSt13_Ios_FmtflagsS_, @function
+_ZStaNRSt13_Ios_FmtflagsS_:
+.LFB1119:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movq	%rdi, -8(%rbp)
+	movl	%esi, -12(%rbp)
+	movq	-8(%rbp), %rax
+	movl	(%rax), %eax
+	movl	-12(%rbp), %edx
+	movl	%edx, %esi
+	movl	%eax, %edi
+	call	_ZStanSt13_Ios_FmtflagsS_
+	movq	-8(%rbp), %rdx
+	movl	%eax, (%rdx)
+	movq	-8(%rbp), %rax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1119:
+	.size	_ZStaNRSt13_Ios_FmtflagsS_, .-_ZStaNRSt13_Ios_FmtflagsS_
+	.section	.text._ZNSt8ios_base4setfESt13_Ios_FmtflagsS0_,"axG",@progbits,_ZNSt8ios_base4setfESt13_Ios_FmtflagsS0_,comdat
+	.align 2
+	.weak	_ZNSt8ios_base4setfESt13_Ios_FmtflagsS0_
+	.type	_ZNSt8ios_base4setfESt13_Ios_FmtflagsS0_, @function
+_ZNSt8ios_base4setfESt13_Ios_FmtflagsS0_:
+.LFB1148:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	%rdi, -24(%rbp)
+	movl	%esi, -28(%rbp)
+	movl	%edx, -32(%rbp)
+	movq	-24(%rbp), %rax
+	movl	24(%rax), %eax
+	movl	%eax, -4(%rbp)
+	movl	-32(%rbp), %eax
+	movl	%eax, %edi
+	call	_ZStcoSt13_Ios_Fmtflags
+	movl	%eax, %edx
+	movq	-24(%rbp), %rax
+	addq	$24, %rax
+	movl	%edx, %esi
+	movq	%rax, %rdi
+	call	_ZStaNRSt13_Ios_FmtflagsS_
+	movl	-32(%rbp), %edx
+	movl	-28(%rbp), %eax
+	movl	%edx, %esi
+	movl	%eax, %edi
+	call	_ZStanSt13_Ios_FmtflagsS_
+	movl	%eax, %edx
+	movq	-24(%rbp), %rax
+	addq	$24, %rax
+	movl	%edx, %esi
+	movq	%rax, %rdi
+	call	_ZStoRRSt13_Ios_FmtflagsS_
+	movl	-4(%rbp), %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1148:
+	.size	_ZNSt8ios_base4setfESt13_Ios_FmtflagsS0_, .-_ZNSt8ios_base4setfESt13_Ios_FmtflagsS0_
+	.section	.text._ZSt3hexRSt8ios_base,"axG",@progbits,_ZSt3hexRSt8ios_base,comdat
+	.weak	_ZSt3hexRSt8ios_base
+	.type	_ZSt3hexRSt8ios_base, @function
+_ZSt3hexRSt8ios_base:
+.LFB1176:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movq	%rdi, -8(%rbp)
+	movq	-8(%rbp), %rax
+	movl	$74, %edx
+	movl	$8, %esi
+	movq	%rax, %rdi
+	call	_ZNSt8ios_base4setfESt13_Ios_FmtflagsS0_
+	movq	-8(%rbp), %rax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1176:
+	.size	_ZSt3hexRSt8ios_base, .-_ZSt3hexRSt8ios_base
+	.local	_ZStL8__ioinit
+	.comm	_ZStL8__ioinit,1,1
+	.section	.rodata
+.LC0:
+	.string	" "
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB1522:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$112, %rsp
+	movl	%edi, -100(%rbp)
+	movq	%rsi, -112(%rbp)
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	movl	$-1, -80(%rbp)
+	movl	$-1, -76(%rbp)
+	movl	$-1, -72(%rbp)
+	movl	$-1, -68(%rbp)
+	movl	$-1, -64(%rbp)
+	movl	$-1, -60(%rbp)
+	movl	$-1, -56(%rbp)
+	movl	$-1, -52(%rbp)
+	movq	$0, -48(%rbp)
+	movq	$0, -40(%rbp)
+	movq	$0, -32(%rbp)
+	movq	$0, -24(%rbp)
+#APP
+# 14 "ast-penalty.cc" 1
+	vzeroupper
+# 0 "" 2
+#NO_APP
+	leaq	-80(%rbp), %rax
+#APP
+# 16 "ast-penalty.cc" 1
+	vcvttps2dq 0(%rax), %ymm2
+# 0 "" 2
+# 17 "ast-penalty.cc" 1
+	vpshufb %ymm2, %ymm1, %ymm2
+# 0 "" 2
+# 18 "ast-penalty.cc" 1
+	vextracti128  $1, %ymm2, %xmm3
+# 0 "" 2
+# 19 "ast-penalty.cc" 1
+	vpor %xmm2, %xmm3, %xmm2
+	leaq	-48(%rbp), %rax
+	movq	%xmm2, 0(%rax)
+# 0 "" 2
+# 23 "ast-penalty.cc" 1
+	vzeroupper
+# 0 "" 2
+#NO_APP
+	movl	$0, -84(%rbp)
+.L17:
+	cmpl	$7, -84(%rbp)
+	jg	.L16
+	leaq	_ZSt3hexRSt8ios_base(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZNSolsEPFRSt8ios_baseS0_E@PLT
+	movq	%rax, %rdx
+	movl	-84(%rbp), %eax
+	cltq
+	movl	-48(%rbp,%rax,4), %eax
+	movl	%eax, %esi
+	movq	%rdx, %rdi
+	call	_ZNSolsEi@PLT
+	leaq	.LC0(%rip), %rsi
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	addl	$1, -84(%rbp)
+	jmp	.L17
+.L16:
+	movq	_ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_@GOTPCREL(%rip), %rax
+	movq	%rax, %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZNSolsEPFRSoS_E@PLT
+	movl	$0, %eax
+	movq	-8(%rbp), %rcx
+	xorq	%fs:40, %rcx
+	je	.L19
+	call	__stack_chk_fail@PLT
+.L19:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1522:
+	.size	main, .-main
+	.type	_Z41__static_initialization_and_destruction_0ii, @function
+_Z41__static_initialization_and_destruction_0ii:
+.LFB2012:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	cmpl	$1, -4(%rbp)
+	jne	.L22
+	cmpl	$65535, -8(%rbp)
+	jne	.L22
+	leaq	_ZStL8__ioinit(%rip), %rdi
+	call	_ZNSt8ios_base4InitC1Ev@PLT
+	leaq	__dso_handle(%rip), %rdx
+	leaq	_ZStL8__ioinit(%rip), %rsi
+	movq	_ZNSt8ios_base4InitD1Ev@GOTPCREL(%rip), %rax
+	movq	%rax, %rdi
+	call	__cxa_atexit@PLT
+.L22:
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2012:
+	.size	_Z41__static_initialization_and_destruction_0ii, .-_Z41__static_initialization_and_destruction_0ii
+	.type	_GLOBAL__sub_I_main, @function
+_GLOBAL__sub_I_main:
+.LFB2013:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$65535, %esi
+	movl	$1, %edi
+	call	_Z41__static_initialization_and_destruction_0ii
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2013:
+	.size	_GLOBAL__sub_I_main, .-_GLOBAL__sub_I_main
+	.section	.init_array,"aw"
+	.align 8
+	.quad	_GLOBAL__sub_I_main
+	.hidden	__dso_handle
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
